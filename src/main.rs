eac5e1979b7a309b990e68a06033103fe5f69567
caleb <lorencalebbaum@gmail.com>
2024-12-02 08:53:19 -0500

    day 1 done and day 2 added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod solution;
+use solution::day1;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Solution day_1_1: {}", day1::part1());
+    println!("Solution day_1_2: {}", day1::part2());
 }
