7519f0b3c89dc503fa54dd982cb6a53803f8a213
caleb <lorencalebbaum@gmail.com>
2024-12-03 09:53:55 -0500

    solve day 3, want to try maps on part2 later

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod solution;
-use solution::{day1, day2};
+use solution::{day1, day2, day3};
 
 fn main() {
     println!("Solution day_1_1: {}", day1::part1());
     println!("Solution day_1_2: {}", day1::part2());
     println!("Solution day_2_1: {}", day2::part1());
     println!("Solution day_2_2: {}", day2::part2());
+    println!("Solution day_3_1: {}", day3::part1());
+    println!("Solution day_3_2: {}", day3::part2());
 }
